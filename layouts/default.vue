1be588004cd58f2577521e5775744936cf136151
Rhio Kim <rhio.kim@gmail.com>
2019-11-11 21:14:02 +0900

    feat: markup list and contents

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,7 +10,7 @@
             width="24"
             height="24"
             viewBox="0 0 24 24"
-            class="flex-none w-5 h-5 mr-3 text-gray-500 hover:text-blue-500"
+            class="flex-none w-5 h-5 text-gray-500 hover:text-blue-500"
           >
             <path
               d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z"
@@ -23,7 +23,7 @@
             width="24"
             height="24"
             viewBox="0 0 24 24"
-            class="flex-none w-5 h-5 mr-3 text-gray-300 hover:text-gray-500"
+            class="flex-none w-5 h-5 text-gray-300 hover:text-gray-500"
           >
             <path
               d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z"
@@ -114,11 +114,117 @@
         </ul>
       </div>
     </div>
-    <div id="list" class="w-1/5 border-r border-gray-200 bg-gray-100 p-5">
-      list
+    <div id="list" class="w-1/5 bg-gray-100 overflow-y-hidden">
+      <div
+        class="flex items-center justify-between border-b border-r border-gray-200 px-8 py-5"
+      >
+        <div class="flex items-center"></div>
+        <div class="flex items-center">
+          <svg
+            fill="currentColor"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            class="flex-none w-5 h-5 mr-3 text-gray-300 hover:text-gray-500"
+          >
+            <path
+              d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z"
+            ></path>
+          </svg>
+          <svg
+            fill="currentColor"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            class="flex-none w-5 h-5 text-gray-300 hover:text-gray-500"
+          >
+            <path
+              d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z"
+            ></path>
+          </svg>
+        </div>
+      </div>
+      <div class="flex justify-between p-2 border-r border-gray-200">
+        <div class="flex">Newest</div>
+      </div>
+      <div class="overflow-y-auto">
+        <ul>
+          <li class="flex h-32 p-5 border-b border-r border-gray-200 active">
+            <div>aaa</div>
+          </li>
+          <li class="flex h-32 p-5 border-b border-r border-gray-200">
+            <div>bbb</div>
+          </li>
+          <li class="flex h-32 p-5 border-b border-r border-gray-200">
+            <div>bbb</div>
+          </li>
+          <li class="flex h-32 p-5 border-b border-r border-gray-200">
+            <div>bbb</div>
+          </li>
+          <li class="flex h-32 p-5 border-b border-r border-gray-200">
+            <div>bbb</div>
+          </li>
+          <li class="flex h-32 p-5 border-b border-r border-gray-200">
+            <div>bbb</div>
+          </li>
+          <li class="flex h-32 p-5 border-b border-r border-gray-200">
+            <div>bbb</div>
+          </li>
+          <li class="flex h-32 p-5 border-b border-r border-gray-200">
+            <div>bbb</div>
+          </li>
+        </ul>
+      </div>
     </div>
     <div id="content" class="w-3/5">
-      <nuxt />
+      <div
+        class="flex items-center justify-between border-b border-r border-gray-200 px-8 py-5"
+      >
+        <div class="flex items-center"></div>
+        <div class="flex items-center">
+          <svg
+            fill="currentColor"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            class="flex-none w-5 h-5 text-gray-300 hover:text-gray-500"
+          >
+            <path
+              d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z"
+            ></path>
+          </svg>
+        </div>
+      </div>
+      <div class="flex justify-between px-8 py-5 border-r border-gray-200">
+        <div class="flex"></div>
+        <div class="flex">
+          <svg
+            fill="currentColor"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            class="flex-none w-5 h-5 mr-3 text-gray-300 hover:text-gray-500"
+          >
+            <path
+              d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z"
+            ></path>
+          </svg>
+          <svg
+            fill="currentColor"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            class="flex-none w-5 h-5 mr-3 text-gray-300 hover:text-gray-500"
+          >
+            <path
+              d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z"
+            ></path>
+          </svg>
+        </div>
+      </div>
+      <div class="overflow-y-auto p-5">
+        <nuxt />
+      </div>
     </div>
   </div>
 </template>
@@ -135,4 +241,7 @@ html, body
 #menu
   // box-shadow: 5px 0px 3px -4px gray
   height: 100%
+
+.active
+  @apply bg-white border-white
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>content</div>
+  <div class="px-10">
+    <div class="text-sm text-gray-500">22th July 2017</div>
+    <h1>Today meeting opened!</h1>
+    <div class="mt-8">
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,4 +24,7 @@
 export default {}
 </script>
 
-<style></style>
+<style lang="sass" scoped>
+h1
+  @apply text-4xl
+</style>
